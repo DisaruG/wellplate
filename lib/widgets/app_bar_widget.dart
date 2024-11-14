95ba0df8025488f947ce7e35617f786342a97aaa
Disaru Gunawardhana <disaru1314@gmail.com>
2024-11-15 01:29:10 +0530

    Update app_bar_widget.dart

--- lib/widgets/app_bar_widget.dart
+++ lib/widgets/app_bar_widget.dart
@@ -2,34 +2,36 @@ import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool isDarkMode;
-  final Function toggleTheme;
+  final VoidCallback toggleTheme;
 
   const CustomAppBar({
-    Key? key,
+    super.key,
     required this.isDarkMode,
     required this.toggleTheme,
-  }) : super(key: key);
-
-  @override
-  Size get preferredSize => const Size.fromHeight(60); // Height of the AppBar
+  });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.white, // AppBar with a white background
-      title: const Text(
+      backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+      title: Text(
         'Dashboard',
-        style: TextStyle(color: Colors.black), // Title on the left
+        style: TextStyle(
+          color: isDarkMode ? Colors.white : Colors.black, // Dynamically set text color
+        ),
       ),
       actions: [
         IconButton(
           icon: Icon(
-            isDarkMode ? Icons.nightlight_round : Icons.wb_sunny, // Toggle icon
-            color: Colors.black, // Icon color
+            isDarkMode ? Icons.nightlight_round : Icons.wb_sunny,
+            color: isDarkMode ? Colors.white : Colors.black, // Dynamically set icon color
           ),
-          onPressed: () => toggleTheme(), // On press, toggle the theme
+          onPressed: toggleTheme,
         ),
       ],
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
