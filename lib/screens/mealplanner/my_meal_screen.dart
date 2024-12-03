80d78aecdb6e3e36eea33e42cfc3b4d47fc3113c
Disaru Gunawardhana <disaru1314@gmail.com>
2024-12-03 22:30:03 +0530

    Create my_meal_screen.dart

---
+++ lib/screens/mealplanner/my_meal_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyMealsScreen extends StatelessWidget {
+  const MyMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Meals'),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to My Meals!',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
