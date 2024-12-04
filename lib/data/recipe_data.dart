a315e797edbc94f5bd7a0388400141022edded5e
Disaru Gunawardhana <disaru1314@gmail.com>
2024-12-05 00:00:48 +0530

    Update recipe_data.dart

--- lib/data/recipe_data.dart
+++ lib/data/recipe_data.dart
@@ -1,5 +1,4 @@
 // recipe_data.dart
-
 class Recipe {
   final String name;
   final String cuisine;
@@ -16,28 +15,13 @@ class Recipe {
   });
 }
 
-// Updated recipe list with no duplicate recipes
+// Recipe data
 final List<Recipe> recipes = [
-  // Italian Recipes
   Recipe(
-    name: "Spaghetti Carbonara",
+    name: "Spaghetti Carbonara (Weight Loss)",
     cuisine: "Italian",
     type: "Non-Vegetarian",
     goal: "Weight Loss",
-    imageAsset: "assets/carbonara.jpg",
-  ),
-  Recipe(
-    name: "Spaghetti Carbonara",
-    cuisine: "Italian",
-    type: "Non-Vegetarian",
-    goal: "Muscle Gain",
-    imageAsset: "assets/carbonara.jpg",
-  ),
-  Recipe(
-    name: "Spaghetti Carbonara",
-    cuisine: "Italian",
-    type: "Non-Vegetarian",
-    goal: "Maintenance",
     imageAsset: "assets/carbonara.jpg",
   ),
   Recipe(
@@ -46,29 +30,6 @@ final List<Recipe> recipes = [
     type: "Vegetarian",
     goal: "Weight Loss",
     imageAsset: "assets/VegetableLasagna.jpg",
-  ),
-  Recipe(
-    name: "Vegetarian Lasagna",
-    cuisine: "Italian",
-    type: "Vegetarian",
-    goal: "Muscle Gain",
-    imageAsset: "assets/VegetableLasagna.jpg",
-  ),
-  Recipe(
-    name: "Margherita Pizza",
-    cuisine: "Italian",
-    type: "Vegetarian",
-    goal: "Balanced",
-    imageAsset: "assets/pizza.jpg",
-  ),
-
-  // Chinese Recipes
-  Recipe(
-    name: "Vegetable Stir Fry",
-    cuisine: "Chinese",
-    type: "Vegetarian",
-    goal: "Weight Loss",
-    imageAsset: "assets/Vegetablefry.jpg",
   ),
   Recipe(
     name: "Vegetable Stir Fry",
@@ -85,15 +46,6 @@ final List<Recipe> recipes = [
     imageAsset: "assets/kungpao.jpg",
   ),
   Recipe(
-    name: "Sweet and Sour Pork",
-    cuisine: "Chinese",
-    type: "Non-Vegetarian",
-    goal: "Low-Calorie",
-    imageAsset: "assets/Sweet.jpg",
-  ),
-
-  // Indian Recipes
-  Recipe(
     name: "Paneer Butter Masala",
     cuisine: "Indian",
     type: "Vegetarian",
@@ -101,50 +53,11 @@ final List<Recipe> recipes = [
     imageAsset: "assets/Masala.jpg",
   ),
   Recipe(
-    name: "Chicken Tikka Masala",
-    cuisine: "Indian",
-    type: "Non-Vegetarian",
-    goal: "Low-Carb",
-    imageAsset: "assets/tikka.jpg",
-  ),
-  Recipe(
-    name: "Dal Tadka",
-    cuisine: "Indian",
-    type: "Vegan",
-    goal: "Weight Loss",
-    imageAsset: "assets/Dal.jpg",
-  ),
-
-  // Mexican Recipes
-  Recipe(
-    name: "Tacos",
+    name: "Tacos (Muscle Gain)",
     cuisine: "Mexican",
     type: "Non-Vegetarian",
     goal: "Muscle Gain",
     imageAsset: "assets/Tacos.jpg",
-  ),
-  Recipe(
-    name: "Vegetarian Quesadilla",
-    cuisine: "Mexican",
-    type: "Vegetarian",
-    goal: "Low-Calorie",
-    imageAsset: "assets/Quesadillas.jpg",
-  ),
-  Recipe(
-    name: "Guacamole with Tortilla Chips",
-    cuisine: "Mexican",
-    type: "Vegan",
-    goal: "Balanced",
-    imageAsset: "assets/Guacamole.jpg",
-  ),
-
-  // American Recipes
-  Recipe(
-    name: "Vegan Burger",
-    cuisine: "American",
-    type: "Vegan",
-    goal: "Muscle Gain",
-    imageAsset: "assets/vegan.jpg",
   ),
   Recipe(
     name: "Classic Cheeseburger",
@@ -154,4 +67,3 @@ final List<Recipe> recipes = [
     imageAsset: "assets/cheeseburger.jpg",
   ),
 ];
-
